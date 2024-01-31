41bf0fbb84df1f7bcf1bf281db926eae25e08d6c
Otavie <otavieokuoyo@gmail.com>
2024-01-31 08:55:02 +0100

    Add joi validateUserMiddleware during registration

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import { register, login } from "../controllers/authentication"
+import { validateUserMiddleware } from "../validators/user.validator"
 
 export default (router: Router) => {
-    router.post('/auth/register', register)
+    router.post('/auth/register', validateUserMiddleware, register)
     router.post('/auth/login', login)
 }
