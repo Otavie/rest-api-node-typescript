bb9223c97cec523e86b325ca674ab63c7b9024ef
Otavie <otavieokuoyo@gmail.com>
2024-01-31 09:39:55 +0100

    sep of concerns. DB function written as a helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,28 @@
 import crypto from 'crypto'
+import mongoose from 'mongoose'
 
 const SECRET_API_KEY = process.env.SECRET
+const DB_URI = process.env.DB_URI
 
 export const random = () => crypto.randomBytes(128).toString('base64')
 
 export const authentication = (salt: string, password: string) => {
     return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET_API_KEY).digest('hex')
+}
+
+export const connectDB = () => {
+    mongoose.Promise = Promise
+    mongoose.connect(DB_URI)
+
+    mongoose.connection.on('connected', () => {
+        console.log('Connected to DB successfully!')
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Disconnected from DB!')
+    })
+
+    mongoose.connection.on('error', (error) => {
+        console.log('Error connecting to the DB', error)
+    })
 }
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,12 @@ import express from 'express'
 import http from 'http'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
-import mongoose from "mongoose"
 import compression from 'compression'
 import cors from 'cors'
+import { connectDB } from './helpers'
 import router from "./routes"
 
-
 const PORT = process.env.PORT
-const DB_URI = process.env.DB_URI
 
 const app = express()
 
@@ -29,19 +27,6 @@ server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
 
-mongoose.Promise = Promise
-mongoose.connect(DB_URI)
-
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to the DB`)
-})
-
-mongoose.connection.on('disconnected', () => {
-    console.log(`Disconnected from the DB`)
-})
-
-mongoose.connection.on('error', (error: Error) => {
-    console.log(`Error connecting to the DB`, error)
-})
+connectDB()
 
 app.use('/', router())
